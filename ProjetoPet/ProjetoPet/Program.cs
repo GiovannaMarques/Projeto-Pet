b634278a1d3b3fd6de3aed947298373553237bd2
Matheus Campanini Mughrabi <matheus.mughrabi@gmail.com>
2022-01-20 18:51:29 -0300

    - Criada classe Tutor e exemplo de uso

--- ProjetoPet/ProjetoPet/Program.cs
+++ ProjetoPet/ProjetoPet/Program.cs
@@ -6,7 +6,29 @@ namespace ProjetoPet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Armazena um espaço na memória Heap para um novo Objeto do tipo "Tutor" e aponta para variável "primeiroTutor" para este novo espaço de memória
+            // O valor padrão do tipo de dado "int" no C# é "zero"
+            // O valor padrão do tipo de dado "string no C# é "null
+            // Por isso, inicialmente, Id = 0, Nome = null, Sobrenome = null
+            Tutor primeiroTutor = new Tutor();
+
+            // A atribuição dos valores desejados para as propriedade acontece nas três linhas a seguir
+            primeiroTutor.Id = 1;
+            primeiroTutor.Nome = "Giovanna";
+            primeiroTutor.Sobrenome = "Marques";
+
+
+
+            // Aqui nós estamos reservando um novo espaço na memória Heap para um novo Objeto do tipo "Tutor" 
+            // Este novo objeto é independente do primeiro que foi criado na linha 13!!
+            // Nós criamos uma variável chamada "segundoTutor" que irá apontar para o novo Objeto criado
+            // Inicialmente os valores das propriedades deste objeto são Id = 0, Nome = null, Sobrenome = null
+            Tutor segundoTutor = new Tutor();
+
+            // A atribuição dos valores desejados para as propriedade acontece nas três linhas a seguir
+            segundoTutor.Id = 2;
+            segundoTutor.Nome = "Matheus";
+            segundoTutor.Sobrenome = "Campanini";
         }
     }
 }
