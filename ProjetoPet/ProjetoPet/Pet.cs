7c993c3b7ad350d67f20f72ce032ce8f1b7eafd5
Giovanna Marques <giovannalimamarques@hotmail.com>
2022-01-27 18:41:30 -0300

    enviando pets

--- ProjetoPet/ProjetoPet/Pet.cs
+++ ProjetoPet/ProjetoPet/Pet.cs
@@ -1,7 +1,23 @@
-﻿namespace ProjetoPet
+﻿using System;
+
+namespace ProjetoPet
 {
     public class Pet
     {
         // colocar propriedades Pet
+
+        public int IdPet;
+        public string NomePet;
+        public DateTime DataDeNascimentoPet;
+        public string Especie;
+        public int IdTutor;
+
+
+        //Id (ex: 1800)
+        //Nome(ex: Meg)
+        //Data de nascimento(ex: 15/08/2016)
+        //Espécie(ex: Cão)
+        //IdTutor(ex: 1555)
+
     }
 }
